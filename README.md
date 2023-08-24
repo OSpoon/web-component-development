79bc0406857b8ca3c4ad956f9deaea6a98730d67
fe-xiaoxin <zxin088@gmail.com>
2023-08-24 14:48:54 +0800

    fix: fix build bug，update docs

--- README.md
+++ README.md
@@ -1,40 +1,94 @@
-# Quark 多组件模式
+# Quark 组件开发
 
-基于本工程，您可以构建属于自己的跨技术栈/无框架 组件。
+基于本工程，您可以构建一系列属于自己的跨技术栈/无框架 组件。
 
-## 如何使用
+## 支持
+
+在 `template-quarkc-component-ts` 的基础上做了以下变更
+
+| 序号 | 描述                            |
+| ---- | ------------------------------- |
+| 1    | 调整 `npm run build` 为批量构建 |
+| 2    | 添加 `npm run new` 创建组件     |
+
+## 初始化
 
 ```
 npm install
 npm run dev
 ```
 
-入口文件为 `src/index.tsx`，这里使用 `vite` 进行开发和生产打包。
+## 1. 生成组件
 
-## 打包产物
+生成名为 MyComponent 的组件
+
+```
+$ npm run new
+
+> cross-platform-components@0.0.1 new
+> plop
+
+? component name please : my-component
+✔  ++ /components/MyComponent/index.tsx
+✔  ++ /components/MyComponent/index.less
+✔  ++ /components/MyComponent/vite-env.d.ts
+```
+
+## 2. 使用组件
+
+```html
+<head>
+  <!-- ① 导入组件 -->
+  <script type="module" src="./components/MyComponent/index.tsx"></script>
+</head>
+<body>
+  <!-- ② 使用组件 -->
+  <my-component text="为跨技术栈而生！"></my-component>
+</body>
+```
+
+## 3. 打包产物
 
 ```
 npm run build
 ```
 
-打包后的产出为： `lib/index.js`和`lib/index.umd.cjs`。
+打包后的产出为： `dist/<component name>/index.js` 和 `dist/<component name>/index.umd.js`。
 
 ```tree
 .
 ├── types
-|     └── install.d.ts
+|     └── index.d.ts
 ├── index.js
-└── index.umd.js
+├── index.umd.js
+└── package.json
 ```
 
-## 使用产物
 
-无论是`Vue`，`React`，`Angular`还是`Jq`项目，该组件都可以被使用。
+## 4. 验证产物
 
-```js
-import "my-component/lib";
+将组件导入路径调整为打包后的产物路径
 
-<my-component count="0" />;
+```html
+<head>
+  <!-- ① 导入组件 -->
+  <script type="module" src="./dist/MyComponent/index.js"></script>
+</head>
+<body>
+  <!-- ② 使用组件 -->
+  <my-component text="为跨技术栈而生！"></my-component>
+</body>
+```
+
+```html
+<head>
+  <!-- ① 导入组件 -->
+  <script type="module" src="./dist/MyComponent/index.umd.js"></script>
+</head>
+<body>
+  <!-- ② 使用组件 -->
+  <my-component text="为跨技术栈而生！"></my-component>
+</body>
 ```
 
 详细文档，请访问：https://quarkc.hellobike.com/#/zh-CN/docs/publishing
--- scripts/components-script.ts
+++ scripts/components-script.ts
@@ -1,91 +1,105 @@
 import path from "node:path";
 import fs from "fs-extra";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import { build, BuildOptions, InlineConfig } from "vite";
-import dts from 'vite-plugin-dts';
+import dts from "vite-plugin-dts";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const componentRootPath = path.resolve(__dirname, "../components/");
 
-const OUTPUT_DTS = 'dist/types';
+const OUTPUT_DTS = "dist/types";
 
-const syncDts = (name) => fs.copySync(
-    path.resolve(`${OUTPUT_DTS}/${name}`, `index.d.ts`),
+const syncDts = (onlyOne, name) =>
+  fs.copySync(
+    path.resolve(
+      // 单个组件生成的 dts 文件缺少 name 目录
+      onlyOne ? `${OUTPUT_DTS}` : `${OUTPUT_DTS}/${name}`,
+      `index.d.ts`
+    ),
     path.resolve(`dist/${name}/types`, `index.d.ts`)
-)
+  );
 
 const clearDts = () => fs.removeSync(OUTPUT_DTS);
 
 const getComponentDirNames = () =>
-    fs.readdirSync(componentRootPath)
-        .filter(filename => {
-            const componentDir = path.resolve(componentRootPath, filename);
-            const isDir = fs.lstatSync(componentDir).isDirectory();
-            return isDir && fs.readdirSync(componentDir).includes("index.tsx");
-        })
+  fs.readdirSync(componentRootPath).filter((filename) => {
+    const componentDir = path.resolve(componentRootPath, filename);
+    const isDir = fs.lstatSync(componentDir).isDirectory();
+    return isDir && fs.readdirSync(componentDir).includes("index.tsx");
+  });
 
 const fileNameConvert = (format, entryName) => {
-    if (format === "es") {
-        return `${entryName}.js`;
-    }
-    return `${entryName}.${format}.js`;
-}
+  if (format === "es") {
+    return `${entryName}.js`;
+  }
+  return `${entryName}.${format}.js`;
+};
 
 const outputPkgFile = (filepath, pkg) => {
-    fs.outputFile(path.resolve(filepath, `package.json`), JSON.stringify(pkg, null, 2), `utf-8`);
-}
+  fs.outputFile(
+    path.resolve(filepath, `package.json`),
+    JSON.stringify(pkg, null, 2),
+    `utf-8`
+  );
+};
 
 const buildAllComponents = async () => {
-    const componentDirNames = getComponentDirNames();
-    for (let name of componentDirNames) {
-        const componentEntry = path.resolve(componentRootPath, name);
-        const outputCompDir = `dist/${name}`;
+  const componentDirNames = getComponentDirNames();
+  if (componentDirNames.length === 0) {
+    console.warn(
+      "❓You haven't created the component yet, just type `npm run dev` to get started"
+    );
+    return;
+  }
+  for (let name of componentDirNames) {
+    const componentEntry = path.resolve(componentRootPath, name);
+    const outputCompDir = `dist/${name}`;
 
-        const buildConfig: BuildOptions = {
-            lib: {
-                name,
-                formats: ["es", "umd"],
-                entry: componentEntry,
-                fileName: (format, entryName) => fileNameConvert(format, entryName),
-            },
-            rollupOptions: {
-                external: ["quarkc"],
-                output: {
-                    dir: outputCompDir,
-                    globals: {
-                        quarkc: "Quarkc",
-                    },
-                },
-            },
-        }
+    const buildConfig: BuildOptions = {
+      lib: {
+        name,
+        formats: ["es", "umd"],
+        entry: componentEntry,
+        fileName: (format, entryName) => fileNameConvert(format, entryName),
+      },
+      rollupOptions: {
+        external: ["quarkc"],
+        output: {
+          dir: outputCompDir,
+          globals: {
+            quarkc: "Quarkc",
+          },
+        },
+      },
+    };
 
-        await build({
-            build: buildConfig,
-            plugins: [dts()]
-        } as InlineConfig);
+    await build({
+      build: buildConfig,
+      plugins: [dts()],
+    } as InlineConfig);
 
-        syncDts(name)
+    syncDts(componentDirNames.length === 1, name);
 
-        outputPkgFile(outputCompDir, {
-            name: `${name.toLocaleLowerCase()}`,
-            main: `index.umd.js`,
-            module: `index.js`,
-            types: `./types/index.d.ts`,
-            exports: {
-                ".": {
-                    import: "./index.js",
-                    require: "./index.umd.js",
-                    types: "./types/index.d.ts"
-                }
-            },
-            dependencies: {
-                quarkc: "^1.0.17"
-            },
-        });
-    }
+    outputPkgFile(outputCompDir, {
+      name: `${name.toLocaleLowerCase()}`,
+      main: `index.umd.js`,
+      module: `index.js`,
+      types: `./types/index.d.ts`,
+      exports: {
+        ".": {
+          import: "./index.js",
+          require: "./index.umd.js",
+          types: "./types/index.d.ts",
+        },
+      },
+      dependencies: {
+        quarkc: "^1.0.17",
+      },
+    });
+  }
 
-    clearDts();
-}
+  clearDts();
+};
 
 buildAllComponents();
