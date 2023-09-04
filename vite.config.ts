0bcbb5010a77225eae91f2cf87bc48e3ea6d246a
小鑫同学 <zxin088@gmail.com>
2023-09-04 23:53:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
+import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({});
+export default defineConfig({
+    plugins: [
+        UnoCSS({
+            mode: 'shadow-dom',
+        })
+    ]
+});
